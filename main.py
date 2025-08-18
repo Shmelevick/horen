3a46666235cc6daac75bd8e09f8a130cbdbfb49b
shmelevik <yaroslav1992@gmail.com>
2025-08-18 19:12:23 +0300

    Remove create_all() for tables

--- main.py
+++ main.py
@@ -5,15 +5,11 @@ from fastapi import FastAPI
 
 from api_v1 import router as router_v1
 from core.config import settings
-from core.models import Base, db_helper
 from users.views import router as users_router
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with db_helper.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-
     yield
 
 
