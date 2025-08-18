df80db2108e75605aef276fe63f1335990a37a6c
shmelevik <yaroslav1992@gmail.com>
2025-08-18 22:31:17 +0300

    add realtionship to post table

--- core/models/post.py
+++ core/models/post.py
@@ -1,10 +1,17 @@
+from typing import TYPE_CHECKING
+
 from sqlalchemy import String, Text, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
+
+if TYPE_CHECKING:
+    from .user import User
 
 
 class Post(Base):
     title: Mapped[str] = mapped_column(String(100))
     body: Mapped[str] = mapped_column(Text, default="", server_default="") # для модели и внутри БД. Лучше вместе
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+
+    user: Mapped["User"] = relationship(back_populates="posts")
