4898de1a7e83bc8cc61a29613ea230401da8b025
shmelevik <yaroslav1992@gmail.com>
2025-08-19 03:58:22 +0300

    add str, repr to posts

--- core/models/post.py
+++ core/models/post.py
@@ -6,12 +6,17 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from .base import Base
 from .mixins import UserRelationMixin
 
-if TYPE_CHECKING:
-    from .user import User # импорт User произойдёт только для анализаторов типов, в рантайме — нет
-
 
 class Post(UserRelationMixin, Base):
     _user_back_populates = "posts"
 
     title: Mapped[str] = mapped_column(String(100))
     body: Mapped[str] = mapped_column(Text, default="", server_default="") # для модели и внутри БД. Лучше вместе
+
+
+    def __str__(self):
+        return f"{self.__class__.__name__}(id={self.id}, username={self.title!r}, user_id={self.user_id})"
+
+
+    def __repr__(self):
+        return str(self)
