39fab7bb2b137298d28bfbfa6412fe12bf03fb73
shmelevik <yaroslav1992@gmail.com>
2025-08-19 03:58:43 +0300

    add str, rept to user.py

--- core/models/user.py
+++ core/models/user.py
@@ -12,6 +12,12 @@ if TYPE_CHECKING: # импорт User произойдёт только для �
 
 class User(Base):
     username: Mapped[str] = mapped_column(String(32), unique=True)
-    
+
     posts: Mapped[list["Post"]] = relationship(back_populates="user")
     profile: Mapped["Profile"] = relationship(back_populates="user")
+
+    def __str__(self):
+        return f"{self.__class__.__name__}(id={self.id}, username={self.username!r})"
+    
+    def __repr__(self):
+        return str(self)
